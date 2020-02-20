9574a10400fa90e9d16be2feb424e893ec183052
selmihamza <selmi.hamza@gmail.com>
2020-02-20 13:06:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import imageInSrc from './imageInSrc.jpg';
 import './App.css';
+import './style.css'
 
 function App() {
+  let myName = <span>hamza</span>
+  let title = (<p className="title">Your name here : {myName}</p>);
+  let imageInpublic = <img className="img" src="/imageInPublic.jpg" alt="imageInpublic"/>
+  let image= <img className="img" src={imageInSrc} alt="imageInSrc"/>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {title}
+      {imageInpublic}
+      {image}
     </div>
   );
 }
